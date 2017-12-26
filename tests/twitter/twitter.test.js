98a41d5a33808fa3bc4e791b32368bf7ce35ba30
TerAleS <terales@users.noreply.github.com>
2017-12-26 18:43:25 +0200

    Add basic test for Twitter resource

---
+++ tests/twitter/twitter.test.js
@@ -0,0 +1,6 @@
+const test = require('ava')
+const validateReceivedImages = require('../validateReceivedImages')
+
+test('loads correct images', t => {
+  return validateReceivedImages('twitter', t)
+})
