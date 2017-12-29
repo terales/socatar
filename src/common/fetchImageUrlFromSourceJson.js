a9891b1c5e79c99b998d71c35b1bcccca7c9bef9
TerAleS <terales@users.noreply.github.com>
2017-12-29 09:52:12 +0200

    Refactor common/fetchImageUrlFromSourceJson to reduce nesting level

--- src/common/fetchImageUrlFromSourceJson.js
+++ src/common/fetchImageUrlFromSourceJson.js
@@ -1,14 +1,13 @@
+// Native Node.js modules
+const promisify = require('util').promisify
+
 // Third party dependencies
 const request = require('request')
 const getProperty = require('lodash.get')
 
 module.exports = async function fetchImageUrlFromSourceJson (requestOptions, propertyPath) {
-  return new Promise((resolve, reject) => {
-    request(requestOptions, (error, response, parsedBody) => {
-      if (error) { reject(new Error(error)) }
-
-      const url = getProperty(parsedBody, propertyPath, '')
-      return url ? resolve(url) : reject(new Error('404'))
-    })
-  })
+  const res = await promisify(request)(requestOptions)
+  const url = getProperty(res.body, propertyPath, '')
+  if (!url) { throw new Error('404') }
+  return url
 }
