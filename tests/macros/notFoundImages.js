6729ceb13af1b69db1b7cec9e3e37475535ffe68
TerAleS <terales@users.noreply.github.com>
2017-12-31 13:17:26 +0200

    Add fallback image, closes #4

--- tests/macros/notFoundImages.js
+++ tests/macros/notFoundImages.js
@@ -1,10 +1,19 @@
+// Native Node.js modules
+const path = require('path')
+const promisify = require('util').promisify
+const fs = require('fs')
+
+// Third party dependencies
 const supertest = require('supertest')
 
+// Local modules
 const app = require('./../../src/app')
+const fallback = path.join(__dirname, '..', '..', 'src', 'public', 'fallback.svg')
 
 module.exports = async function notFoundImages (t, source) {
   const res = await supertest(app)
     .get(`/${source}/definiteryWrongUserIdHere`)
 
   t.is(res.statusCode, 404)
+  t.deepEqual(res.body, await promisify(fs.readFile)(fallback))
 }
