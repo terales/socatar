7be8c370569c913ef7c87d0ddd8bf77fcca551b3
TerAleS <terales@users.noreply.github.com>
2018-01-04 20:24:56 +0200

    Make CI run for PR from external contributor wihtout security breach, closes #55

--- tests/baseAllSourcesAcceptance.test.js
+++ tests/baseAllSourcesAcceptance.test.js
@@ -1,14 +1,22 @@
+// Third party dependencies
 const test = require('ava')
 const path = require('path')
 
+// Local modules
+const sources = require(path.join(__dirname, '..', 'src', 'sources', 'index'))
+
+// Test macros
 const validateReceivedImages = require('./macros/validateReceivedImages')
 const notFoundImages = require('./macros/notFoundImages')
 const cacheControlHeader = require('./macros/cacheControlHeader')
 
-const sources = require(path.join(__dirname, '..', 'src', 'sources', 'index'))
+// Test helpers
+const filterOurSourcesWithSecureCredentials = require('./helpers/filterOurSourcesWithSecureCredentials')
 
-Object.keys(sources).forEach(source => {
-  test(source + ':validateReceivedImages', validateReceivedImages, source)
-  test(source + ':cacheControlHeader', cacheControlHeader, source)
-  test(source + ':notFoundImages', notFoundImages, source)
-})
+Object.keys(sources)
+  .filter(source => filterOurSourcesWithSecureCredentials(source))
+  .forEach(source => {
+    test(source + ':validateReceivedImages', validateReceivedImages, source)
+    test(source + ':cacheControlHeader', cacheControlHeader, source)
+    test(source + ':notFoundImages', notFoundImages, source)
+  })
