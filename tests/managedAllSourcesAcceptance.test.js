79898448896934cf6a4e4441dacef83ff37b86ed
TerAleS <terales@users.noreply.github.com>
2018-01-10 21:42:30 +0200

    Skip managed tier test suite on PR from forks, #55

--- tests/managedAllSourcesAcceptance.test.js
+++ tests/managedAllSourcesAcceptance.test.js
@@ -2,14 +2,19 @@
 const test = require('ava')
 const path = require('path')
 
+// Local modules
 const validateReceivedImages = require('./managed/validateReceivedImages')
 const notFoundImages = require('./managed/notFoundImages')
 const cacheControlHeader = require('./managed/cacheControlHeader')
 
 const sources = require(path.join(__dirname, '..', 'src', 'sources', 'index'))
 
-Object.keys(sources).forEach(source => {
-  test(source + ':validateReceivedImages', validateReceivedImages, source)
-  test(source + ':cacheControlHeader', cacheControlHeader, source)
-  test(source + ':notFoundImages - managed', notFoundImages, source)
-})
+if (process.env.CLOUDINARY_URL) {
+  Object.keys(sources).forEach(source => {
+    test(source + ':validateReceivedImages', validateReceivedImages, source)
+    test(source + ':cacheControlHeader', cacheControlHeader, source)
+    test(source + ':notFoundImages - managed', notFoundImages, source)
+  })
+} else {
+  console.log('Managet tier acceptance test suite skipped because of hidden/empty `CLOUDINARY_URL` environment variable')
+}
