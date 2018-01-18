1f7bf934a081f18c51300a2f807ce84d96b29c46
TerAleS <terales@users.noreply.github.com>
2018-01-18 06:06:06 +0200

    Use default ImageMagic from Travis CI container

--- tests/helpers/imageMagicCompare.js
+++ tests/helpers/imageMagicCompare.js
@@ -1,10 +1,16 @@
-
+const os = require('os')
 const { spawn } = require('child_process')
 
 module.exports = function imageMagicCompare (sample, received) {
   return new Promise((resolve, reject) => {
     // magick compare -fuzz 2% -metric AE {{sample}} {{received}} null:
-    const imagemagic = spawn('magick', ['compare', '-fuzz', '2%', '-metric', 'AE', sample, received, 'null:'])
+
+    if (os.platform() === 'win32') {
+      const imagemagic = spawn('magick', ['compare', '-fuzz', '2%', '-metric', 'AE', sample, received, 'null:'])
+    } else {
+      const imagemagic = spawn('compare', ['-fuzz', '2%', '-metric', 'AE', sample, received, 'null:'])
+    }
+
 
     imagemagic.stderr.on('data', data => {
       if (Number.isNaN(data)) { reject(new Error(`imagemagic error:\n  ${data}`)) }
