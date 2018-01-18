29097986a8e5637b41a6e6edad001f16ed339001
TerAleS <terales@users.noreply.github.com>
2018-01-18 05:15:23 +0200

    Switch to ImageMagic for image comparison in test suite, #66

---
+++ tests/helpers/imageMagicCompare.js
@@ -0,0 +1,17 @@
+
+const { spawn } = require('child_process')
+
+module.exports = function imageMagicCompare (sample, received) {
+  return new Promise((resolve, reject) => {
+    // magick compare -fuzz 2% -metric AE {{sample}} {{received}} null:
+    const imagemagic = spawn('magick', ['compare', '-fuzz', '2%', '-metric', 'AE', sample, received, 'null:'])
+
+    imagemagic.stderr.on('data', data => {
+      if (Number.isNaN(data)) { reject(new Error(`imagemagic error:\n  ${data}`)) }
+      resolve(parseInt(data) === 0)
+    })
+
+    imagemagic.on('close', code => code !== 0 ? reject(new Error(`imagemagic process exited with code: ${code}`)) : null)
+    imagemagic.on('error', error => reject(new Error('Failed to imagemagic subprocess\n' + error)))
+  })
+}
--- tests/macros/validateReceivedImages.js
+++ tests/macros/validateReceivedImages.js
@@ -1,36 +1,29 @@
 // Native Node.js modules
 const fs = require('fs')
 const path = require('path')
-const promisify = require('util').promisify
 
 // Third party dependencies
 const supertest = require('supertest')
 const rimraf = require('rimraf')
-const pixelmatch = require('pixelmatch')
-const jpegParser = require('jpeg-js')
 
 // Local modules
 const app = require('./../../src/app')
 const getSourceSamples = require('./../helpers/getSourceSamples')
+const imageMagicCompare = require('./../helpers/imageMagicCompare')
 
-module.exports = function validateReceivedImages (t, source) {
+module.exports = async function validateReceivedImages (t, source) {
   const samples = getSourceSamples(source)
   const receivablesDir = path.join(__dirname, '..', 'sources', source, 'receivables')
-
   clearDir(receivablesDir)
 
-  return Promise.all(samples.files.map(file =>
-            Promise.all([
-              path.join(samples.dir, file),
-              getReceivedImage(receivablesDir, source, file)
-            ]).then(images => {
-              const sample = parseImage(images[0])
-              const received = parseImage(images[1])
-              const diff = pixelmatch(sample, received, null, sample.width, sample.height)
-              t.is(diff, 0, file + ' is different')
-            })
-        )
-    )
+  t.plan(samples.files.length)
+
+  for (let file of samples.files) {
+    const sample = path.join(samples.dir, file)
+    const received = await getReceivedImage(receivablesDir, source, file)
+    const isEqual = await imageMagicCompare(sample, received)
+    t.true(isEqual, 'Received unexpected image: ' + file)
+  }
 }
 
 function clearDir (dir) {
@@ -45,11 +38,4 @@ function getReceivedImage (receivablesDir, source, file) {
         .pipe(fs.createWriteStream(path.join(receivablesDir, file)))
         .on('finish', () => resolve(path.join(receivablesDir, file)))
   })
-}
-
-async function parseImage (file) {
-  return jpegParser.decode(
-    await promisify(fs.readFile)(file),
-    true
-  )
 }
