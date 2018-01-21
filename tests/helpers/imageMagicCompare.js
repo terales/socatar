979d3825b5e625542033348930cce588137abc3b
Alexander <terales@users.noreply.github.com>
2018-01-21 19:52:29 +0200

    Stabilize acceptance tests suite, closes #66
    * Switch to ImageMagic for image comparison in test suite, #66
    * Treat all images as binary to stabilize samples
    * Prepare CLI script to automatically update all samples

---
+++ tests/helpers/imageMagicCompare.js
@@ -0,0 +1,20 @@
+const os = require('os')
+const { spawn } = require('child_process')
+
+module.exports = function imageMagicCompare (sample, received) {
+  return new Promise((resolve, reject) => {
+    // magick compare -fuzz 5% -metric AE {{sample}} {{received}} null:
+    const options = ['-fuzz', '5%', '-metric', 'AE', sample, received, 'null:']
+    const imagemagic = os.platform() === 'win32'
+      ? spawn('magick', ['compare'].concat(options))
+      : spawn('compare', options)
+
+    imagemagic.stderr.on('data', data => {
+      if (Number.isNaN(data)) { reject(new Error(`imagemagic error:\n  ${data}`)) }
+      resolve(parseInt(data) === 0)
+    })
+
+    imagemagic.on('close', code => code !== 0 ? reject(new Error(`imagemagic process exited with code: ${code}`)) : null)
+    imagemagic.on('error', error => reject(new Error('Failed to imagemagic subprocess\n' + error)))
+  })
+}
