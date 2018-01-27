d679e846516d8abf6375c5635e83dbbcd132e78c
TerAleS <terales@users.noreply.github.com>
2018-01-27 20:03:46 +0200

    Cache not found images

--- src/middleware/imageNotFoundHandler.js
+++ src/middleware/imageNotFoundHandler.js
@@ -6,5 +6,6 @@ const fallback = path.join(__dirname, '..', 'public', 'fallback.svg')
 
 module.exports = function imageNotFoundHandler (err, req, res, next) {
   if (res.headersSent || err.message !== '404') { return next(err) }
+  res.header('cache-control', 'public, max-age=1209600, no-transform')
   res.status(404).sendFile(fallback)
 }
