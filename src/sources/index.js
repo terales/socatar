4c91d1a232024f7fbc6bb4fdf97811b98d31552b
Alexander <terales@users.noreply.github.com>
2018-07-06 11:36:05 +0300

    Update dependencies

--- src/sources/index.js
+++ src/sources/index.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
 module.exports = require('fs')
-    .readdirSync(__dirname)
-    .filter(file => !(file.match(/\.js$/) === null || file === 'index.js'))
-    .map(file => file.slice(0, -3))
-    .reduce((sources, source) => {
-      sources[source] = require(path.join(__dirname, source))
-      return sources
-    }, {})
+  .readdirSync(__dirname)
+  .filter(file => !(file.match(/\.js$/) === null || file === 'index.js'))
+  .map(file => file.slice(0, -3))
+  .reduce((sources, source) => {
+    sources[source] = require(path.join(__dirname, source))
+    return sources
+  }, {})
